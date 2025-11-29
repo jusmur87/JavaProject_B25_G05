d29a379db7ec4c4cef9b6cad383d6c4d6f9c35f2
jusmur87 <justyna.murdza1234@gmail.com>
2025-11-29 18:07:58 -0500

    tasks 123

--- src/main/java/group5/Task_2.java
+++ src/main/java/group5/Task_2.java
@@ -5,4 +5,17 @@ Create an array of five integer values. After the array is created, use a loop t
 print the average of all the elements.
  */
 public class Task_2 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum +=numbers[i];
+
+        }
+        double average = (double) sum/ numbers.length;
+
+        System.out.println("Average: " + average);
+    }
 }
--- src/main/java/group5/Task_3.java
+++ src/main/java/group5/Task_3.java
@@ -5,4 +5,20 @@ Create an array to store the scores of six students in a class. Use a loop to co
 how many scores are above 80.
  */
 public class Task_3 {
+    public static void main(String[] args) {
+
+        int[] scores = {75, 80, 91, 83, 71, 95};
+
+        int countAbove80 = 0;
+
+        for (int i = 0; i < scores.length; i++) {
+
+            if (scores[i] > 80) {
+              countAbove80++;
+            }
+
+        }
+        System.out.println("Number of scores above 80: " + countAbove80);
+    }
+
 }
